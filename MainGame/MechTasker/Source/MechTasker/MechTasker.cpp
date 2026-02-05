134fedcf87fbe77cc30a121f50ab4f5fae0bab90
Edvard <mitori0211@gmail.com>
2026-02-05 15:12:20 +0100

    Added new source project
    Added new main game project

---
+++ MainGame/MechTasker/Source/MechTasker/MechTasker.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MechTasker.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MechTasker, "MechTasker" );
