5559f8514bb9c1c0a5fdf7a1ac1ec2f1dcad6c71
Candidate_514_23DPRO101 <splunch18@gmail.com>
2026-02-10 16:14:23 +0100

    save game class and deleted binary files

---
+++ MainGame/Source/MechTasker/SaveGameClass.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SaveGameClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHTASKER_API USaveGameClass : public USaveGame
+{
+	GENERATED_BODY()
+	
+
+};
